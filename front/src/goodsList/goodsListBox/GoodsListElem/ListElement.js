68351a364ffdfc3bbd2982cfdcf1e9076c14e0c4
Lucy Nekrasova <l.w.nekrasova@gmail.com>
2022-06-03 12:24:53 +0300

    added icons for list elem: edit and delete

--- front/src/goodsList/goodsListBox/GoodsListElem/ListElement.js
+++ front/src/goodsList/goodsListBox/GoodsListElem/ListElement.js
@@ -7,7 +7,7 @@ function ListElement() {
     <>
       <div className='listElement' id='1'>
         <input className='element checkbox'  name='1-check' value='no' type='checkbox' id='1-check' onclick='' />
-        <label for='1-check'></label>
+        <label className='element' for='1-check'></label>
         <p className='element text'>text</p>
         <div className='element icon edit'></div>
         <div className='element icon delete'></div>
