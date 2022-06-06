7360d1ae83b71b59a14cb50e8809ad47c0db332a
Lucy Nekrasova <l.w.nekrasova@gmail.com>
2022-06-06 15:37:51 +0300

    add btn for adding info to goodsList

--- front/src/goodsList/goodsListInput/InputAddBtn.js
+++ front/src/goodsList/goodsListInput/InputAddBtn.js
@@ -1,11 +1,12 @@
 import React from "react"
 import './goodsListInput.scss'
+import './add.svg'
 
 function InputAddBtn() {
   return (
     <>
       <div className="InputAddBtn">
-        
+        <button type='button' id='addBtn'></button>
       </div>
     </>
   )
