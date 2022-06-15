7f0d3d839b0af3cf93d203673f6f1487cad6d8aa
Lucy Nekrasova <l.w.nekrasova@gmail.com>
2022-06-15 13:29:22 +0300

    list item styles fixed

--- front/src/goodsList/goodsListBox/GoodsListElem/ListElement.js
+++ front/src/goodsList/goodsListBox/GoodsListElem/ListElement.js
@@ -7,18 +7,20 @@ function ListElement(props) {
   const id = props.id ? props.id : Date.now() 
   const text = props.text 
   
+  const checkboxStyle = {opacity: 0, display: 'none'}
 
   return (
     <>
       <div className={`${styles.ListElement}`} id={id}>
         <input className={`${styles.element} ${check.checkbox}`} 
-          name='check1' 
+          name={`check${id}`} 
           value='no' 
           type='checkbox' 
-          id={`${check.check1}`} 
+          id={`${check.check}${id}`} 
+          style={checkboxStyle}
           onClick={null}           
         />
-        <label className={`${styles.element}`} htmlFor={`${check.check1}`}></label>
+        <label className={`${styles.element}`} htmlFor={`${check.check}${id}`}></label>
         <p className={`${styles.element} ${styles.text}`}>{text}</p>
         <div className={`${styles.element} ${styles.icon} ${styles.edit}`}></div>
         <div className={`${styles.element} ${styles.icon} ${styles.delete}`}></div>
