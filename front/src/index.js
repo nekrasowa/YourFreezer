b301291136289755e04ed5aeddbfa2b86d99c516
Lucy Nekrasova <l.w.nekrasova@gmail.com>
2022-06-16 13:37:23 +0300

    add file state with array and functions

--- front/src/index.js
+++ front/src/index.js
@@ -1,19 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-
-const goods = [
-  {id: 1, text: 'bananas'},
-  {id: 2, text: 'apple'},
-  {id: 3, text: 'meat'},
-  {id: 4, text: 'pork'},
-  
-]
-
-const deleteElementOfList = (id) => {
-  console.log('[elementOfGoods]:', id)
-  // console.log('[elementOfGoods]:', elem)
-}
+import { goods, deleteElementOfList } from './state'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
---
+++ front/src/state.js
@@ -0,0 +1,13 @@
+export const goods = [
+  {id: 1, text: 'bananas'},
+  {id: 2, text: 'apple'},
+  {id: 3, text: 'meat'},
+  {id: 4, text: 'pork'},
+  
+]
+
+export const deleteElementOfList = (id) => {
+  console.log('[elementOfGoods]:', id)
+}
+
+
